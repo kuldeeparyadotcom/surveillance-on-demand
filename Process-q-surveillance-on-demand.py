bfbd611b9bc09b92e9333d31599d3d8f5f4eb8c7
KD <kuldeeparyadotcom@gmail.com>
2018-01-29 00:13:27 -0600

    Add ability to keep polling associated Amazon SQS queue

--- Process-q-surveillance-on-demand.py
+++ Process-q-surveillance-on-demand.py
@@ -6,6 +6,7 @@ Purpose - Click a picture using Raspi Cam module for every message in the queue
 
 import boto3
 import picamera
+import time
 
 #Get service resource
 sqs = boto3.resource('sqs')
@@ -16,11 +17,16 @@ queue = sqs.get_queue_by_name(QueueName='q-surveillance-on-demand')
 #Get your camera
 camera = picamera.PiCamera()
 
-#Process message by simply clicking a picture using raspi cam module
-for message in queue.receive_messages():
-    print(message)
-    #Click a picture in response to queue message
-    camera.capture('captured_images/living-room.jpg')
+#Keep polling queue for new messages
+while True:
+    #Process message by simply clicking a picture using raspi cam module
+    for message in queue.receive_messages():
+        print(message)
+        #Click a picture in response to queue message
+        camera.capture('captured_images/living-room.jpg')
 
-    #Let the queue know that message is processed
-    message.delete()
+        #Let the queue know that message is processed
+        message.delete()
+        
+        #wait for 10 seconds
+        time.sleep(10)
