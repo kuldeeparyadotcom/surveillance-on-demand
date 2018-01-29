09248ba2539acbe966dbf62dd45a458bd983095e
KD <kuldeeparyadotcom@gmail.com>
2018-01-29 00:23:11 -0600

    Capture timestamp in names of captured images

--- Process-q-surveillance-on-demand.py
+++ Process-q-surveillance-on-demand.py
@@ -7,6 +7,7 @@ Purpose - Click a picture using Raspi Cam module for every message in the queue
 import boto3
 import picamera
 import time
+import datetime
 
 #Get service resource
 sqs = boto3.resource('sqs')
@@ -19,11 +20,15 @@ camera = picamera.PiCamera()
 
 #Keep polling queue for new messages
 while True:
+    #Capture timestamp
+    ts = time.time()
+    image_suffix = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S')
+    image_name = "living-room_" + image_suffix + ".jpg"
     #Process message by simply clicking a picture using raspi cam module
     for message in queue.receive_messages():
         print(message)
         #Click a picture in response to queue message
-        camera.capture('captured_images/living-room.jpg')
+        camera.capture('captured_images/' + image_name)
 
         #Let the queue know that message is processed
         message.delete()
